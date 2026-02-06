c8b4ed0b3737a6cca511368a92a87f9f8d5127c2
Fareen Garg <fareengarg13092005@gmail.com>
2026-02-06 10:54:06 +0530

    we are connecting the database

---
+++ config/database.js
@@ -0,0 +1,20 @@
+// getting the instance of mongoose library..
+const mongoose = require("mongoose")
+
+
+// all the configuration of .env file will be loaded into the process object
+// means process.env.port = 4000 and process.env.database_url = mongodb://127.0.0.1:27017/blogAppDatabase
+require("dotenv").config();
+
+
+// function for connecting the database
+const connectWithDB = ()=>{
+   mongoose.connect(process.env.database_url)
+   .then(console.log("Database has been connected sucessfully"))
+   .catch((error)=>
+{
+    console.log("Database connection has not been established")
+    console.log(error)
+    process.exit(1)
+})
+};
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express")
+
+const app  = express();
+app.listen(3000,()=>{
+    console.log("app has started sucessfully")
+})
+
+app.get('/',(req,res)=>{
+    res.send("hello we are making a blogging app!")
+})
